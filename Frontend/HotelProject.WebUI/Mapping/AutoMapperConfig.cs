7c66c83e49160052ddf0f9c78a0251b43f8a707b
oğuzcan kandemir <oguzcankandemir@hotmail.com>
2023-09-28 00:07:46 +0300

    [feature] Staff done.

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -6,6 +6,8 @@ using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.RoomDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
 
 namespace HotelProject.WebUI.Mapping
 {
@@ -16,13 +18,26 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<ResultServiceDto, Service>().ReverseMap();
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
             CreateMap<CreateServiceDto, Service>().ReverseMap();
-            CreateMap<RoomAddDto, Room>();
-            CreateMap<Room, RoomAddDto>();
-            CreateMap<UpdateRoomDto, Room>().ReverseMap();
+
             CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
             CreateMap<LoginUserDto, AppUser>().ReverseMap();
+
             CreateMap<ResultAboutDto, About>().ReverseMap();
             CreateMap<UpdateAboutDto, About>().ReverseMap();
+
+            CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
+           
+            CreateMap<ResultStaffDto, Staff>().ReverseMap();
+
+            CreateMap<UpdateRoomDto, Room>().ReverseMap();
+
+            CreateMap<RoomAddDto, Room>();
+            CreateMap<Room, RoomAddDto>();
+           
+      
+          
+          
+          
         }
     }
 }
